a771549c2c95549a64196e87de03f4400e333a89
Pedro Jandre <pedrootaviosoriano@gmail.com>
2021-04-06 10:34:24 -0300

    Requisição funcionando

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.post('/sendWPP', (req, res) => {
+app.get('/sendWPP', function (req, res) {
     const accountSid = 'AC6d518caf79f7155886c74cf244140ee5';
     const authToken = '552e53b7779c14ad57f9ccf136138055';
     const client = require('twilio')(accountSid, authToken);
+    const mensagem = req.query.msg;
+    const numero = req.query.num;
     client.messages
         .create({
-            body: 'Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/',
+            body: mensagem,
             from: 'whatsapp:+14155238886',
-            to: 'whatsapp:+553199650413'
+            to: `whatsapp:+${numero}`
         })
-        .then(message => res.send(`A mensagem para ${JSON.stringify(message.to)} foi enviada`))
+        .then(message => res.send(`Mensagem: ${mensagem}\n enviada para o numero +${numero}`))
         .done();
         
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
