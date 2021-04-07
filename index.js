fa4f464b4b14a1814877b1b7a5ac189c9f8cf5ed
Pedro Jandre <pedrootaviosoriano@gmail.com>
2021-04-07 10:15:44 -0300

    Suporte para mensagem sms

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ app.get('/sendWPP', function (req, res) {
         .done();
         
 })
+app.get('/sendSMS', function (req, res) {
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
+    const mensagem = req.query.msg;
+    const numero = req.query.num;
+    client.messages
+        .create({
+            body: mensagem,
+            messagingServiceSid: 'MG98c396b81b118f3e4910f978e2dcbb19',      
+            to: `+${numero}`
+        })
+        .then(message => res.send(`Mensagem: ${mensagem}\n enviada para o numero ${numero}`))
+        .done();
+        
+})
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
